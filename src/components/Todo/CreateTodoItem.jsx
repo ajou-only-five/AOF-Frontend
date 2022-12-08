d5e77567b15c8d1e67a5ff849a923588dba52e3c
Gyun Lee <gyunini@gmail.com>
2022-12-08 19:19:24 +0900

    새로운 투두 아이템 생성 컴포넌트 개발

--- src/components/Todo/CreateTodoItem.jsx
+++ src/components/Todo/CreateTodoItem.jsx
@@ -10,6 +10,9 @@ function CreateTodoItem(props) {
     const createNewItem = () =>{
         props.clicked(true)
     }
+    const backward = () => {
+        props.clicked(false);
+    }
     const Enroll = async (e) => {
         e.preventDefault();
         if(content === '' || startAt === '' || endtAt === '') {
@@ -25,25 +28,25 @@ function CreateTodoItem(props) {
             console.log(v);
             })
             .catch((err) => alert(err));
+            props.clicked(false);
+            // props.setIsLogined(true);
         }
-        props.clicked(false);
-        props.setIsLogined(true);
     }
 
     return (
         <div>
             <div>
-                <button onClick={createNewItem}>새로운 todo 아이템 생성</button>
+                { !props.state? <button onClick={createNewItem}>새로운 todo 아이템 생성</button> : <button onClick={backward}>뒤로가기</button> }
             </div>
             {props.state ? (
-                <div>
-                    <form>
+                <div className="container">
+                    <form className="container">
                         <label>추가할 일정의 제목</label>
-                        <input type='text' value={content} onChange={(e) => { setContent(e.currentTarget.value) }}></input>
+                        <input type='text' placeholder="할 일 1-1" value={content} onChange={(e) => { setContent(e.currentTarget.value) }}></input>
                         <label>일정 시작 시간</label>
-                        <input type='text' value={startAt} onChange={(e) => { setStartAt(e.currentTarget.value) }}></input>
+                        <input type='text' placeholder="2022-10-25 11:00:00" value={startAt} onChange={(e) => { setStartAt(e.currentTarget.value) }}></input>
                         <label>일정 종료 시간</label>
-                        <input type='text' value={endtAt} onChange={(e) => { setEndAt(e.currentTarget.value) }}></input>
+                        <input type='text' placeholder="2022-10-25 12:00:00" value={endtAt} onChange={(e) => { setEndAt(e.currentTarget.value) }}></input>
                         <button onClick={(e) => Enroll(e)}>등록</button>
                     </form>
                 </div>
